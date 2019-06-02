73959053fc477bd0c8d0ab08ed6e6d7bb4b1ab69
mr-myself <keisuke@mr-myself.com>
2019-06-02 10:57:51 +0900

    initial

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//http.HandleFunc("/", handler)
+	//http.ListenAndServe(":8080", nil)
+
+	for {
+		fmt.Println("Hello world!")
+
+		time.Sleep(time.Second * 1)
+	}
+}
+
+//func handler(w http.ResponseWriter, r *http.Request) {
+//    fmt.Fprintf(w, "Hello, &World!")
+//}
