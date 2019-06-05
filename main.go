57c7d2732f168c678e9cc93624ec2119674caa0a
mr-myself <keisuke@mr-myself.com>
2019-06-06 08:21:08 +0900

    prepared k8s basement

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
 func main() {
-	//http.HandleFunc("/", handler)
-	//http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 
-	for {
-		fmt.Println("Hello world!")
+	//for {
+	//    fmt.Println("Hello world!")
 
-		time.Sleep(time.Second * 1)
-	}
+	//    time.Sleep(time.Second * 1)
+	//}
 }
 
-//func handler(w http.ResponseWriter, r *http.Request) {
-//    fmt.Fprintf(w, "Hello, &World!")
-//}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
